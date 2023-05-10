d0b418768696a793d8f2b5df0c9905084c78d3f8
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2023-05-11 01:35:48 +0900

    [Gold II] Title: 버블 소트, Time: 1676 ms, Memory: 82080 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		mData[] A = new mData[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = new mData(Integer.parseInt(br.readLine()), i); 
+		}
+		Arrays.sort(A);
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			if (A[i].index - i > max) {
+				max = A[i].index - i;
+			}
+		}
+		System.out.println(max + 1);
+	}
+
+}
+
+class mData implements Comparable<mData> {
+	int value;
+	int index;
+	
+	public mData(int value, int index) {
+		this.value = value;
+		this.index = index;
+	}
+	
+	@Override
+	public int compareTo(mData o) {
+		return this.value - o.value;
+	}
+}
