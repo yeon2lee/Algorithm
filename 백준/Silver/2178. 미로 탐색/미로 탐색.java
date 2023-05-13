1c1cbc67a7e9b9cbb1a36667b9c8012ff55c8a70
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2023-05-13 13:53:03 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 15472 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	static int[] dx = {0, 1, 0, -1}; // top, right, down, left
+	static int[] dy = {-1, 0, 1, 0};
+	static boolean[][] visited;
+	static int[][] A;
+	static int N, M;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		A = new int[N][M];
+		visited = new boolean[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			String line = st.nextToken();
+			for (int j = 0; j < M; j++) {
+				A[i][j] = Integer.parseInt(line.substring(j, j + 1));
+			}
+		}
+		BFS(0, 0);
+		System.out.println(A[N - 1][M - 1]);
+	}
+
+	public static void BFS(int i, int j) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {i, j});
+		visited[i][j] = true;
+		while (!queue.isEmpty()) {
+			int[] now = queue.poll();
+			for (int k = 0; k < 4; k++) { // 상하좌우 탐색
+				int x = now[0] + dx[k];
+				int y = now[1] + dy[k];
+				if (x >= 0 && y >= 0 && x < N && y < M) { // 좌표 유효성 검사
+					if (A[x][y] != 0 && !visited[x][y]) { // 갈 수 있는 칸 && 방문 검사하기
+						visited[x][y] = true;
+						A[x][y] = A[now[0]][now[1]] + 1; // 깊이 업데이트
+						queue.add(new int[] {x, y});
+					}
+				}
+			}
+		}
+	}
+}
