9c3c02f1d0eedac732b78c7e37bbfe52f5c7ca88
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2023-05-17 11:41:53 +0900

    [Bronze I] Title: 최소공배수, Time: 188 ms, Memory: 14912 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int result = a * b / gcd(a, b);
+			System.out.println(result);
+		}
+	}
+	
+	public static int gcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		} else {
+			return gcd(b, a % b);
+		}
+	}
+
+}
