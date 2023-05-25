2e19963d074a26885dcd5b3c2f40bb2fee84ff9a
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2023-05-25 11:33:03 +0900

    [Silver I] Title: 트리 순회, Time: 120 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,43 +1,39 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int[][] child; // 트리의 자식 노드 저장
 
-	static int n;
-	static int[][] child;
-	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		child = new int[n + 1][2];
-		
-		// l은 v의 왼쪽 노드, r은 v의 오른쪽 노드 
-		for (int i = 0; i < n; i++) {
-			char v = scan.next().charAt(0);
-			char l = scan.next().charAt(0);
-			char r = scan.next().charAt(0);
-			
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // 노드 개수
+		child = new int[N + 1][2];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			char node = st.nextToken().charAt(0);
+			char left = st.nextToken().charAt(0);
+			char right = st.nextToken().charAt(0);
+
 			/*
-			 * child[v 노드값][0]은 v 노드의 왼쪽 노드 값 저장 
-			 * child[v 노드값][1]은 v 노드의 오른쪽 노드 값 저장
+			 * child[node][0] == 0인 경우를 자식 노드가 없다는 의미로 쓰기 위해서, 노드 번호를 0번부터가 아니라 1번부터 시작한다.
+			 * 그래서 (node - 'A')가 아니라 (node - 'A' + 1). (left - 'A')가 아니라 (left - 'A' + 1)을 해줌.
 			 */
-			if (l != '.') {
-				child[v - 'A' + 1][0] = l - 'A' + 1;
+			if (left != '.') {
+				child[node - 'A' + 1][0] = left - 'A' + 1;
 			}
-			if (r != '.') {
-				child[v - 'A' + 1][1] = r - 'A' + 1;
+			if (right != '.') {
+				child[node - 'A' + 1][1] = right - 'A' + 1;
 			}
 		}
 		
-		/*
-		 * 항상 A가 루트 노드 -> preorder(1) 호출
-		 */
-		preorder(1);
+		preorder(1); // 1번 노드부터 순회 시작
 		System.out.println();
 		inorder(1);
 		System.out.println();
 		postorder(1);
+		System.out.println();
 	}
-	
+
 	public static void preorder(int node) {
 		if (node == 0) {
 			return;
@@ -47,7 +43,7 @@ public class Main {
 		preorder(child[node][0]);
 		preorder(child[node][1]);
 	}
-	
+
 	public static void inorder(int node) {
 		if (node == 0) {
 			return;
@@ -57,7 +53,7 @@ public class Main {
 		System.out.print((char) (node + 'A' - 1));
 		inorder(child[node][1]);
 	}
-	
+
 	public static void postorder(int node) {
 		if (node == 0) {
 			return;
