bc5b467a044ca313e5f1562dbd7da064c54934ae
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2023-05-28 18:17:31 +0900

    [Silver V] Title: 다리 놓기, Time: 160 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int MAX = 30;
+	static int[][] D;
+	public static void main(String[] args) throws IOException {
+		D = new int[MAX + 1][MAX + 1];
+		for (int i = 0; i <= MAX; i++) {
+			D[i][0] = 1;
+			D[i][i] = 1;
+		}
+		
+		for (int i = 2; i <= MAX; i++) {
+			for (int j = 1; j < i; j++) {
+				D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
+			}
+		}
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());	
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			System.out.println(D[M][N]);
+		}
+	}
+}
