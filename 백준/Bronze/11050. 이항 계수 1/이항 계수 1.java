a07cccc63aaf4317eba25b055bdeeac81ae32795
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2023-05-28 17:29:33 +0900

    [Bronze I] Title: 이항 계수 1, Time: 120 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -2,20 +2,26 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	static int[][] D;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		System.out.println(f(N, K));
+		D = new int[N + 1][N + 1];
+
+		for (int i = 0; i <= N; i++) {
+			D[i][1] = i;
+			D[i][0] = 1;
+			D[i][i] = 1;
+		}
+		
+		for (int i = 2; i <= N; i++) {
+			for (int j = 1; j < i; j++) {
+				D[i][j] = D[i - 1][j] + D[i - 1][j - 1]; // 조합 기본 점화식
+			}
+		}
+		System.out.println(D[N][K]);
 	}
 	
-	public static int f(int n, int k) {
-		if (n == k || k == 0) {
-			return 1;
-		}
-		return f(n - 1, k - 1) + f(n - 1, k);
-	}
-
 }
