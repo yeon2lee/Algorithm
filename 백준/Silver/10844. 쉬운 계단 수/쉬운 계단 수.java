3313bcf6d4deac8444d2803534f64e9d52d93d88
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2023-06-02 15:24:01 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -11,18 +11,15 @@ public class Main {
 		}
 		
 		for (int i = 2; i <= N; i++) {
-			for (int j = 0; j <= 9; j++) {
-				if (j == 0) {
-					D[i][j] = D[i - 1][j + 1] % mod;
-				} else if (j == 9) {
-					D[i][j] = D[i - 1][j - 1] % mod;
-				} else {
-					D[i][j] = (D[i - 1][j - 1] + D[i - 1][j + 1]) % mod;
-				}
+			D[i][0] = D[i - 1][1];
+			D[i][9] = D[i - 1][8];
+			
+			for (int j = 1; j <= 8; j++) {
+				D[i][j] = (D[i - 1][j - 1] + D[i - 1][j + 1]) % mod;
 			}
 		}
 		
-		int result = 0;
+		long result = 0;
 		for (int i = 0; i <= 9; i++) {
 			result = (result + D[N][i]) % mod;
 		}
