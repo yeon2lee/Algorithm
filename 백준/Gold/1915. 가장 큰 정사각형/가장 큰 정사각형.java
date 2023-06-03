86e2567f875477459d7def6149ec340a5c25d11a
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2023-06-03 22:48:05 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 324 ms, Memory: 25472 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -5,28 +5,24 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		int[][] arr = new int[n + 1][m + 1];
-		for (int i = 1; i <= n; i++) {
-			String str = br.readLine();
-			for (int j = 1; j <= m; j++) {
-				arr[i][j] = Integer.parseInt(String.valueOf(str.charAt(j - 1)));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] D = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			char[] temp = br.readLine().toCharArray();
+			for (int j = 0; j < M; j++) {
+				D[i][j] = temp[j] - '0';
 			}
 		}
 		
-		// 정사각형 크기 구하기
 		int max = 0;
-		int[][] dp = new int[n + 1][m + 1];
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= m; j++) {
-				if (arr[i][j] == 1) {
-					dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
-					max = Math.max(max, dp[i][j]);
-				}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (D[i][j] == 1 && i > 0 && j > 0) {
+					D[i][j] = Math.min(D[i - 1][j - 1], Math.min(D[i - 1][j], D[i][j - 1])) + 1;
+				} 
+				max = Math.max(max, D[i][j]);
 			}
 		}
 		System.out.println(max * max);
