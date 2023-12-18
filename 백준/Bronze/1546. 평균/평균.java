7043f9035904a2ef2714c29415c62cf8d8b203b0
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2023-12-18 14:42:07 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -5,21 +5,22 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] scores = new int[N];
-		int M = -1;
-		for (int i = 0; i < N; i++) {
+		int[] scores = new int[n];
+		int max = -1;
+		for (int i = 0; i < n; i++) {
 			scores[i] = Integer.parseInt(st.nextToken());
-			M = Math.max(M, scores[i]); // 최댓값 구하기
+			if (scores[i] > max) {
+				max = scores[i];
+			}
 		}
 		
 		double sum = 0;
-		for (int i = 0; i < N; i++) {
-			sum += (double) scores[i] / M * 100;
+		for (int i = 0; i < n; i++) {
+			sum += (double) scores[i] / max * 100;
 		}
-		
-		System.out.println(sum / N);
+		System.out.println(sum / n);
 	}
 
 }
