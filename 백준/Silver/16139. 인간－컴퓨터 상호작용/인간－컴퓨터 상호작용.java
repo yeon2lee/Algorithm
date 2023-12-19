707bc4a3049ef732659c03a26272e5fdccd4874b
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2023-12-19 10:08:52 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 968 ms, Memory: 117032 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -9,32 +9,40 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String str = br.readLine();
-		int l = str.length();
-		int q = Integer.parseInt(br.readLine());
+		String s = br.readLine(); //문자열
+		int q = Integer.parseInt(br.readLine()); //질문 수
+		int l=s.length();
 		
-		// 누적 합 배열 구하기
-		int[][] S = new int[l + 1][26];
-		for (int i = 0; i < l; i++) {
-			char ch = str.charAt(i);
-			for (int j = 1; j < l + 1; j++) {
-				if (str.charAt(j - 1) == ch) {
-					S[j][ch - 'a'] = S[j - 1][ch - 'a'] + 1;
-				} else {
-					S[j][ch - 'a'] = S[j - 1][ch - 'a'];
-				}
+		//누적합 기록을 위한 배열 선언
+		int[][] memos = new int[l+1][26];
+		//처음 값 횟수 증가
+		memos[1][s.charAt(0)-'a']++;
+		//나머지 문자 탐색
+		for(int i=2;i<=l;i++) {
+			//탐색 중인 문자
+			int idx = s.charAt(i-1)-'a';
+			//알파벳(a-z)탐색
+			for(int j=0;j<26;j++) {
+				//알파벳의 이전 누적 합
+				int before = memos[i-1][j];
+				//알파벳과 탐색 중인 문자가 같으면 이전값+1, 아니면 이전값과 같음
+				memos[i][j]=j==idx?before+1:before;
 			}
 		}
-		
-		for (int i = 0; i < q; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			char alphabet = st.nextToken().charAt(0);
-			int start = Integer.parseInt(st.nextToken()) + 1;
-			int end = Integer.parseInt(st.nextToken()) + 1;
-			
-			bw.append(S[end][alphabet - 'a'] - S[start - 1][alphabet - 'a'] + "\n");
+		//문제 읽기
+		StringTokenizer st;
+		for(int i=0;i<q;i++) {
+			st=new StringTokenizer(br.readLine());
+			//문제 문자
+			int idx=st.nextToken().charAt(0)-'a';
+			//시작점
+			int start=Integer.parseInt(st.nextToken())+1;
+			//끝점
+			int end=Integer.parseInt(st.nextToken())+1;
+			//끝점의 누적합 - 시작점 바로 이전의 누적합 = 구간의 문자 반복값
+			bw.append((memos[end][idx]-memos[start-1][idx])+"\n");
 		}
-		bw.flush();
 		bw.close();
+		br.close();
 	}
 }
