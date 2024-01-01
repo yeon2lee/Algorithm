7065bf25b8a38ee3347962c51fdc8b9b728bbcce
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-01-01 20:39:00 +0900

    [Silver II] Title: DNA 비밀번호, Time: 296 ms, Memory: 21976 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -2,108 +2,108 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int checkArr[]; // 비밀번호 체크 배열
-	static int myArr[]; // 현재 상태 배열
-	static int checkSecret; // 몇 개의 문자와 관련된 개수를 충족했는지 판단하는 변수
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int S = Integer.parseInt(st.nextToken());
 		int P = Integer.parseInt(st.nextToken());
-		char[] A = new char[S];
-		checkArr = new int[4];
-		myArr = new int[4];
-		checkSecret = 0;
-		
-		A = br.readLine().toCharArray();
+		P--;
+		char[] str = br.readLine().toCharArray();
+		//{‘A’, ‘C’, ‘G’, ‘T’}의 등장횟수 입력
+		int[] minCount = new int[4];
+		int check = 0;
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < 4; i++) {
-			checkArr[i] = Integer.parseInt(st.nextToken());
+			minCount[i] = Integer.parseInt(st.nextToken());
+			if (minCount[i] == 0) {
+				check++;
+			}
+		}
+		
+		//문자가 등장하는 횟수 구하기
+		int[] count = new int[4];
+		int result = 0;
+		for (int i = 0; i <= P; i++) {
+			if (str[i] == 'A') {
+				count[0]++;
+				if (count[0] == minCount[0]) {
+					check++;
+				}
+			} else if (str[i] == 'C') {
+				count[1]++;
+				if (count[1] == minCount[1]) {
+					check++;
+				}
+			} else if (str[i] == 'G') {
+				count[2]++;
+				if (count[2] == minCount[2]) {
+					check++;
+				}
+			} else if (str[i] == 'T') {
+				count[3]++;
+				if (count[3] == minCount[3]) {
+					check++;
+				}
+			}
+		}
+		if (check >= 4) {
+			result++;
+		}
+		
+		for (int i = 1; P + i < S; i++) {
 			
-			if (checkArr[i] == 0) {
-				checkSecret++;
+				if (str[i - 1] == 'A') {
+					if (count[0] == minCount[0]) {
+						check--;
+					}
+					count[0]--;
+				} else if (str[i - 1] == 'C') {
+					if (count[1] == minCount[1]) {
+						check--;
+					}
+					count[1]--;
+					
+				} else if (str[i - 1] == 'G') {
+					if (count[2] == minCount[2]) {
+						check--;
+					}
+					count[2]--;
+				} else if (str[i - 1] == 'T') {
+					if (count[3] == minCount[3]) {
+						check--;
+					}
+					count[3]--;
+				}
+			
+			
+			if (str[P + i] == 'A') {
+				count[0]++;
+				if (count[0] == minCount[0]) {
+					check++;
+				}
+			} else if (str[P + i] == 'C') {
+				count[1]++;
+				if (count[1] == minCount[1]) {
+					check++;
+				}
+			} else if (str[P + i] == 'G') {
+				count[2]++;
+				if (count[2] == minCount[2]) {
+					check++;
+				}
+			} else if (str[P + i] == 'T') {
+				count[3]++;
+				if (count[3] == minCount[3]) {
+					check++;
+				}
+			}
+			
+			if (check >= 4) {
+				result++;
 			}
 		}
-		
-		for (int i = 0; i < P; i++) {
-			Add(A[i]);
-		}
-		
-		int count = 0;
-		if (checkSecret == 4) {
-			count++;
-		}
-		
-		// 슬라이딩 윈도우
-		for (int i = P; i < S; i++) {
-			int j = i - P;
-			Add(A[i]);
-			Remove(A[j]);
-			if (checkSecret == 4) {
-				count++;
-			}
-		}
-		
-		System.out.println(count);
-	}
 	
-	public static void Add(char c) {
-		switch(c) {
-			case 'A':
-				myArr[0]++;
-				if (myArr[0] == checkArr[0]) {
-					checkSecret++;
-				}
-				break;
-			case 'C':
-				myArr[1]++;
-				if (myArr[1] == checkArr[1]) {
-					checkSecret++;
-				}
-				break;
-			case 'G':
-				myArr[2]++;
-				if (myArr[2] == checkArr[2]) {
-					checkSecret++;
-				}
-				break;
-			case 'T':
-				myArr[3]++;
-				if (myArr[3] == checkArr[3]) {
-					checkSecret++;
-				}
-				break;
-		}
-	}
-	
-	public static void Remove(char c) {
-		switch(c) {
-		case 'A':
-			if (myArr[0] == checkArr[0]) {
-				checkSecret--;
-			}
-			myArr[0]--;
-			break;
-		case 'C':
-			if (myArr[1] == checkArr[1]) {
-				checkSecret--;
-			}
-			myArr[1]--;
-			break;
-		case 'G':
-			if (myArr[2] == checkArr[2]) {
-				checkSecret--;
-			}
-			myArr[2]--;
-			break;
-		case 'T':
-			if (myArr[3] == checkArr[3]) {
-				checkSecret--;
-			}
-			myArr[3]--;
-			break;
-	    }
+		System.out.println(result);
 	}
 
 }
