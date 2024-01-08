5b1a79d406a8d36c9b998487b12bb299b77b3f6d
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-01-08 14:27:52 +0900

    [Silver II] Title: 스택 수열, Time: 340 ms, Memory: 25764 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -4,42 +4,41 @@ import java.util.Stack;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		// 수열의 개수 N, 수열 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		// 수열의 개수와 수열 입력
 		int N = Integer.parseInt(br.readLine());
 		int[] A = new int[N];
 		for (int i = 0; i < N; i++) {
 			A[i] = Integer.parseInt(br.readLine());
 		}
-		
-		// 스택 연산 수행
+
+		// 스택으로 오름차순 수열 만들기
 		Stack<Integer> stack = new Stack<>();
+		boolean success = true; // 오름차순 수열 만들기 성공여부
+		int insert = 1; // 수열에 삽입해야 할 수
+		int index = 0; // 확인할 수열의 인덱스
 		StringBuffer bf = new StringBuffer();
-		int num = 1;
-		boolean result = true;
-		for (int i = 0; i < N; i++) {
-			if (A[i] >= num) {
-				while (A[i] >= num) {
-					stack.push(num++);
-					bf.append("+\n");
-				}
-				stack.pop();
-				bf.append("-\n");
-			} else { // A[i] < num
-				int n = stack.pop();
-				if (n > A[i]) {
-					System.out.println("NO");
-					result = false;
-					break;
-				} else {
+		while (index < N) {
+			if (insert <= A[index]) {
+				stack.push(insert++);
+				bf.append("+\n");
+			} else { // insert > A[i]
+				int now = stack.peek(); //스택 가장 위에 있는 수
+				if (now == A[index]) {
+					stack.pop();
 					bf.append("-\n");
+					index++;
+				} else {
+					success = false;
+					break;
 				}
 			}
-			
 		}
-		if (result) {
+
+		if (success) {
 			System.out.println(bf.toString());
+		} else {
+			System.out.println("NO");
 		}
 	}
 
