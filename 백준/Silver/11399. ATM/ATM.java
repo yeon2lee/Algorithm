d5a68a9eb4b60105cf885a2c83b245841e43e3d1
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-01-19 00:50:20 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,38 +13,14 @@ public class Main {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
 
-		// 삽입 정렬 구현
-		for (int i = 0; i < N; i++) {
-			int index = i;
-			for (int j = i - 1; j >= 0; j--) {
-				if (A[i] < A[j]) {
-					index = j;
-				} else {
-					break;
-				}
-			}
-			// shift
-			int tmp = A[i];
-			for (int j = i; j > index; j--) {
-				A[j] = A[j - 1];
-			}
-			A[index] = tmp;
-		}
-
-		// 각 사람이 인출하는 데 필요한 시간의 합
-		// 합 배열 만들기
-		int[] S = new int[N];
-		S[0] = A[0];
-		for (int i = 1; i < N; i++) {
-			S[i] = S[i - 1] + A[i];
-		}
-		
-		// 합 배열 총합 구하기
+		Arrays.sort(A);
+		int result = 0;
 		int sum = 0;
 		for (int i = 0; i < N; i++) {
-			sum += S[i];
+			sum += A[i];
+			result += sum;
 		}
-		System.out.println(sum);
+		System.out.println(result);
 	}
 
 }
