9f1599a7b65b1cba6fb8659e86de429c82fb04b0
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-01-20 00:30:31 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		String str = "";
+		str += "         ,r'\"7\n";
+		str += "r`-_   ,'  ,/\n";
+		str += " \\. \". L_r'\n";
+		str += "   `~\\/\n";
+		str += "      |\n";
+		str += "      |\n";
+		System.out.println(str);
+	}
+}
