6b343dd5053a8639f328757e0dcb395ac2933862
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-01-24 16:05:48 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2632 ms, Memory: 377064 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,45 +1,48 @@
 import java.io.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 		int[] A = new int[N];
 		for (int i = 0; i < N; i++) {
 			A[i] = Integer.parseInt(br.readLine());
 		}
-		br.close();
+	
+		//기수정렬
 		radixSort(A, 5);
+		
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < N; i++) {
-			bw.write(A[i] + "\n");
+			sb.append(A[i] + "\n");
 		}
-		bw.flush();
-		bw.close();
+		System.out.println(sb.toString());
 	}
 
-	// 기수 정렬 함수 구현하기
-	public static void radixSort(int[] A, int maxSize) {
+	private static void radixSort(int[] A, int maxSize) {
 		int[] output = new int[A.length];
-		int digit = 1;
+		int digit = 1; //자릿수
 		int count = 0;
-		while (count != maxSize) { // 최대 자리수 만큼 반복
-			int[] bucket = new int[10];
+		while (count != maxSize) { //최대 자릿수만큼 반복하기
+			int[] bucket = new int[10]; //현재 자릿수들의 분포를 합 배열의 형태로 알려주는 배열
 			for (int i = 0; i < A.length; i++) {
-				bucket[(A[i] / digit) % 10]++; // 일의 자리 부터 시작
+				bucket[(A[i] / digit) % 10]++; //일의 자리부터 시작하기
 			}
-			for (int i = 1; i < 10; i++) { // 합배열을 이용하여 index 계산
+			for (int i = 1; i < 10; i++) { //합 배열을 이용해 index 계산하기
 				bucket[i] += bucket[i - 1];
 			}
-			for (int i = A.length - 1; i >= 0; i--) { // 현재 자리수 기준으로 정렬하기
-				output[bucket[(A[i] / digit % 10)] - 1] = A[i];
+			for (int i = A.length - 1; i >= 0; i--) { //현재 자릿수를 기준으로 정렬하기
+				output[bucket[(A[i] / digit) % 10] - 1] = A[i];
 				bucket[(A[i] / digit) % 10]--;
 			}
 			for (int i = 0; i < A.length; i++) {
-				A[i] = output[i]; // 다음 자리 수 이동을 위해 현재 자리수 기준 정렬 데이터 저장
+				//다음 자릿수를 이동하기 위해 현재 자릿수 기준 정렬 데이터 저장하기
+				A[i] = output[i];
 			}
-			digit = digit * 10; // 자리수 증가
+			digit *= 10; //자릿수 증가시키기
 			count++;
 		}
 	}
+
 }
