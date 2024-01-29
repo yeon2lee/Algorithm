13cff3c431e9c7b4016fcda1f767f3a967160e51
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-01-30 00:28:21 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		boolean[][] paper = new boolean[100][100];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < 10; j++) {
+				for (int k = 0; k < 10; k++) {
+					paper[x + j][y + k] = true;
+				}
+			}
+		}
+		int result = 0;
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				if (paper[i][j]) {
+					result++;
+				}
+			}
+		}
+		System.out.println(result);
+	}
+
+}
