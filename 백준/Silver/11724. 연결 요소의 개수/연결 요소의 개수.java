b4770fc17532b14d33dc6a1036f2674b1b788ee7
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-02-13 16:26:17 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 704 ms, Memory: 142164 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,10 +1,10 @@
 import java.io.*;
-import java.util.StringTokenizer;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 	static ArrayList<Integer>[] A;
-	static boolean visited[];
+	static boolean[] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -12,37 +12,37 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 		A = new ArrayList[N + 1];
 		visited = new boolean[N + 1];
-		for (int i = 1; i < N + 1; i++) {
+		for (int i = 1; i <= N; i++) {
 			A[i] = new ArrayList<Integer>();
 		}
-		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			A[s].add(e);
-			A[e].add(s);
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			A[V].add(E);
+			A[E].add(V);
 		}
-		int count = 0;
-		for (int i = 1; i < N + 1; i++) {
+		
+		int count = 0; 
+		for (int i = 1; i <= N; i++) {
 			if (!visited[i]) {
 				count++;
 				DFS(i);
 			}
 		}
+		
 		System.out.println(count);
 	}
-	
-	static void DFS(int v) {
+
+	public static void DFS(int v) {
 		if (visited[v]) {
 			return;
 		}
 		visited[v] = true;
 		for (int i : A[v]) {
-			if (visited[i] == false) {
+			if (!visited[i]) {
 				DFS(i);
 			}
 		}
 	}
-
 }
