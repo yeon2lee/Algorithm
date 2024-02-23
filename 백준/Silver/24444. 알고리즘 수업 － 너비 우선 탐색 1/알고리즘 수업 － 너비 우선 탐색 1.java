90767263092e6b92ee8a08b62a471af7ef5345a2
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-02-23 16:38:24 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1856 ms, Memory: 99952 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static ArrayList<Integer>[] A;
+	static int[] visitOrder;
+	static int count = 1;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		A = new ArrayList[N + 1];
+		visitOrder = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			A[i] = new ArrayList<Integer>();
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int S = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			A[S].add(E);
+			A[E].add(S);
+		}
+		//오름차순 정렬
+		for (int i = 1; i <= N; i++) {
+			Collections.sort(A[i]);
+		}
+		
+		BFS(R);
+		for (int i = 1; i <= N; i++) {
+			System.out.println(visitOrder[i]);
+		}
+	}
+
+	public static void BFS(int node) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(node);
+		visitOrder[node] = count++;
+
+		while (!queue.isEmpty()) {
+			int now = queue.poll();
+			for (int i : A[now]) {
+				if (visitOrder[i] == 0) {
+					visitOrder[i] = count++;
+					queue.add(i);
+				}
+			}
+		}
+	}
+
+}
