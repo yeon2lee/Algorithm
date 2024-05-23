09d3af3ac3c6f84266d016706e811d3b96ce6fd5
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-05-24 00:27:16 +0900

    [Silver V] Title: 다리 놓기, Time: 120 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -4,28 +4,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int MAX = 30;
-	static int[][] D;
-	public static void main(String[] args) throws IOException {
-		D = new int[MAX + 1][MAX + 1];
-		for (int i = 0; i <= MAX; i++) {
-			D[i][0] = 1;
-			D[i][i] = 1;
-		}
-		
-		for (int i = 2; i <= MAX; i++) {
-			for (int j = 1; j < i; j++) {
-				D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
-			}
-		}
-	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for (int i = 0; i < T; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());	
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			System.out.println(D[M][N]);
-		}
-	}
+
+    static long[][] memo; //조합값을 저장하는 배열
+
+    public static void main(String[] args) throws IOException {
+
+        //조합 초기값 
+        memo = new long[31][31];
+        for (int i = 0; i < 31; i++) {
+                memo[0][i] = 1;
+                memo[i][0] = 1;
+                memo[i][i] = 1;
+        }
+
+        //테스트 케이스 개수 T, 강의 서쪽 사이트 개수 N, 동쪽 사이트 개수 M 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            sb.append(combination(M, N));
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static long combination(int n, int m) {
+        if (memo[n][m] == 0) {
+            memo[n][m] = combination(n - 1, m - 1) + combination(n - 1, m);
+        }
+        return memo[n][m];
+    }
 }
