b657af924e424d10b63e77bb14a5773c3e130972
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-05-28 11:28:36 +0900

    [Silver I] Title: 포도주 시식, Time: 144 ms, Memory: 14932 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,43 +1,29 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 public class Main {
-	
-	static Integer[] dp;
-	static int[] arr;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		dp = new Integer[N + 1];
-		arr = new int[N + 1];
-		
-		for(int i = 1; i < N + 1; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		dp[0] = 0;
-		dp[1] = arr[1];
-		
-		/*
-		 *  (N이 1로 주어질 수 있으므로 이럴 때를 위해 조건식을 달아둔다.
-		 *  또한 dp[2]는 어떤 경우에도 첫 번째와 두 번째를 합한 것이 최댓값이다. 
-		 */
-		if(N > 1) {
-			dp[2] = arr[1] + arr[2];
-		}
-		
-		System.out.println(recur(N));
-	}
-	
-	static int recur(int N) {
-		
-		if(dp[N] == null) {
-			dp[N] = Math.max(Math.max(recur(N - 2), recur(N - 3) + arr[N - 1]) + arr[N], recur(N - 1));
-		}
-		
-		return dp[N];
-	}
+
+    public static void main(String[] args) throws IOException {
+
+        //포도주 잔의 개수 N, 포도주의 양 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+
+        //최대로 마실 수 있는 포도주의 양 구하기
+        int[] dp = new int[N + 1];
+        dp[1] = A[1];
+        if (N > 1) {
+            dp[2] = A[1] + A[2];
+        }
+        for (int i = 3; i <= N; i++) {
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + A[i], dp[i - 3] + A[i - 1] + A[i]));
+        }
+
+        System.out.println(dp[N]);
+    }
 }
