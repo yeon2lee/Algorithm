a322d81ea75750bec379455cd4e9cece3ba7c635
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-06-29 23:12:06 +0900

    [Gold IV] Title: 플로이드, Time: 528 ms, Memory: 44752 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[][] distance = new int[N + 1][N + 1];
+        //인접 행렬 초기화
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) {
+                    distance[i][j] = 0;
+                } else {
+                    distance[i][j] = 10000001;
+                }
+            }
+        }
+        //버스 비용 정보 저장
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            if (distance[s][e] > v) {
+                distance[s][e] = v;
+            }
+        }
+
+        //플로이드-워셜 알고리즘 수행
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (distance[i][j] > distance[i][k] + distance[k][j]) {
+                        distance[i][j] = distance[i][k] + distance[k][j];
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (distance[i][j] == 10000001) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print(distance[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
