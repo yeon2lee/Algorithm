5bd0ee1e9efbb48968defa6839ea868ed9210c56
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-06-30 06:59:38 +0900

    [Silver I] Title: 경로 찾기, Time: 296 ms, Memory: 18212 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -10,14 +10,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] distance = new int[N][N];
+        //인접 행렬 입력
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                int v = Integer.parseInt(st.nextToken());
-                distance[i][j] = v;
+                distance[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        //플로이드-워셜 알고리즘 수행
         for (int k = 0; k < N; k++) {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
@@ -27,13 +28,13 @@ public class Main {
                 }
             }
         }
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 System.out.print(distance[i][j] + " ");
             }
             System.out.println();
         }
-
     }
 
 }
