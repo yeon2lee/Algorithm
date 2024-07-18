1b325d3bb6f67df3ebe51dec0be5f1459cfa8e02
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-07-18 12:42:59 +0900

    [Silver II] Title: 부분수열의 합, Time: 136 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,38 +1,40 @@
-import java.util.Scanner;
- 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
-    static int N, S, count=0;
-    static int[] arr;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N= sc.nextInt();
-        S= sc.nextInt();
-        arr = new int[N];
-        for (int i = 0; i <N ; i++) {
-            arr[i] =  sc.nextInt();
+
+    static int N, S, count = 0;
+    static int[] A;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
- 
-        dfs(0,0);
- 
-        if(S==0){// count 합이 0인 경우 공집합도 포함되므로 그 수를 하나 빼주고 출력
+
+        dfs(0, 0);
+
+        if (S == 0) { //공집합인 경우 제외
             count--;
-            System.out.println(count);
-        }else {
-            System.out.println(count);
         }
- 
- 
+        System.out.println(count);
     }
- 
-    private static void dfs(int v , int su){
-        if(v==N){// dfs로 돌며 누적시키다가 위치를 나타내는 v이 마지막 위치로 오면 원하는 값인지 아닌지 체크하여 count
-            if(su == S){
+
+    private static void dfs(int index, int sum) {
+        if (index == N) {
+            if (sum == S) {
                 count++;
             }
             return;
         }
-        // 부분수열, 지금 위치의 원소를 선택하거, 선택하지 않거나
-        dfs(v+1, su+arr[v]); // 지금 위치의 원소를 선택
-        dfs(v+1, su); // 선택하지 않음.
+
+        dfs(index + 1, sum + A[index]);
+        dfs(index + 1, sum);
     }
+
 }
