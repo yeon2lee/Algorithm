00faf8f61d2674fb0fafd5c8dac9c37fc34ea4bb
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2024-10-10 21:02:47 +0900

    [level 2] Title: 조이스틱, Time: 0.09 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int move = name.length() - 1; // 오른쪽으로 쭉 간 횟수
+        
+        for (int i = 0; i < name.length(); i++) {
+            answer += Math.min(name.charAt(i) - 'A', 26 - (name.charAt(i) - 'A')); //상,하 알파벳 맞추기
+            if (i < name.length() - 1 && name.charAt(i + 1) == 'A') {
+                int endA = i + 1;
+                while (endA < name.length() && name.charAt(endA) == 'A')
+                    endA++;
+                move = Math.min(move, i * 2 + (name.length() - endA)); // 오른쪽으로 갔다 다시 왼쪽으로 꺾기
+                move = Math.min(move, i + (name.length() - endA) * 2); // 왼쪽으로 갔다 다시 오른쪽으로 꺾기
+            }
+        }
+        return answer + move;
+    }
+}
