5847c9047ea28f79aa69d650b6d28bc9e83cab3d
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2025-03-27 20:52:59 +0900

    [level 3] Title: 불량 사용자, Time: 409.09 ms, Memory: 403 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(String[] user_id, String[] banned_id) {
+        Set<Set<String>> set = new HashSet<>();
+        Set<String> answer = new HashSet<>();
+        dfs(user_id, banned_id,set, answer, 0);
+        
+        return set.size();
+    }
+    
+    public void dfs(String[] user_id, String[] banned_id, Set<Set<String>> set, Set<String> answer, int depth) {
+        if (depth == banned_id.length) {
+            set.add(new HashSet<>(answer));
+            return;
+        }
+        
+        for (int i = 0 ; i < user_id.length ; i++) {
+            if (user_id[i].matches(banned_id[depth].replace("*",".")) && !answer.contains(user_id[i])) {
+                answer.add(user_id[i]);
+                dfs(user_id, banned_id, set, answer, depth+1);
+                answer.remove(user_id[i]); 
+            }
+        }    
+    }
+}
