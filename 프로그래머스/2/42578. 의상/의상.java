138bb06feda360e8d34bc11df87b255646bbdc1f
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2025-04-08 18:12:36 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,16 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
         HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < clothes.length; i++) {
-            if (!map.containsKey(clothes[i][1])) {
-                map.put(clothes[i][1], 1);
-            } else {
-                map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
-            }
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         
+        int answer = 1;
         for (String key : map.keySet()) {
             answer *= map.get(key) + 1;
         }
