bc22673752f3527f0ca5b4869cea164da68046d7
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2025-04-21 09:49:27 +0900

    [level 2] Title: 더 맵게, Time: 1863.19 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,22 +3,27 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int value : scoville) {
-            queue.add(value);
+        for (int s : scoville) {
+            queue.add(s);
         }
         
+        return getCount(queue, K);
+    }
+    
+    private int getCount(PriorityQueue<Integer> queue, int K) {
         int answer = 0;
-        while (queue.peek() < K && queue.size() > 1) {
+        while (queue.size() > 1) {
+            int peek = queue.peek();
+            if (peek >= K) {
+                return answer;
+            }
+            
             int first = queue.poll();
             int second = queue.poll();
-            int tmp = first + second * 2;
-            queue.add(tmp);
+            int newScoville = first + second * 2;
+            queue.add(newScoville);
             answer++;
         }
-        if (queue.peek() < K) {
-            answer = -1;
-        }
-        
-        return answer;
+        return queue.peek() >= K ? answer : -1;
     }
 }
