e1f1eb9413635f9fd9116fe82536410985e27cbd
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2025-06-24 17:34:09 +0900

    [level 2] Title: 타겟 넘버, Time: 7.73 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,20 @@
+class Solution {
+    static int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+    
+    private void dfs(int[] numbers, int target, int sum, int depth) {
+        if (depth == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
+        } 
+        
+        dfs(numbers, target, sum + numbers[depth], depth + 1);
+        dfs(numbers, target, sum - numbers[depth], depth + 1);
+    }
+}
