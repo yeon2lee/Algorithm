9bcadc669183c4bc367f1708a70b47cbf0138ff1
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2025-09-22 20:58:12 +0900

    [level 3] Title: 여행경로, Time: 12.25 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,36 +1,46 @@
 import java.util.*;
 
 class Solution {
-    boolean[] visited;
-    ArrayList<String> allRoute; 
+    
+    static boolean[] visited;
+    static String[] answer;
+    static boolean found = false;
     
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
-        int cnt = 0;
+        
         visited = new boolean[tickets.length];
-        allRoute = new ArrayList<>();
         
-        dfs("ICN", "ICN", tickets, cnt);
+        // 티켓을 출발지→도착지 기준으로 정렬
+        Arrays.sort(tickets, (a, b) -> {
+            if (a[0].equals(b[0])) {
+                return a[1].compareTo(b[1]); // 출발지가 같으면 도착지 기준
+            }
+            return a[0].compareTo(b[0]);
+        });
         
-        Collections.sort(allRoute);
-        answer = allRoute.get(0).split(" ");
+        dfs("ICN", "ICN", tickets, 0);
         
         return answer;
     }
     
-    public void dfs(String start, String route, String[][] tickets, int cnt){
-        if(cnt == tickets.length){
-            allRoute.add(route);
+    public void dfs(String start, String route, String[][] tickets, int depth){
+        if (found) {
             return;
         }
         
-        for(int i=0; i<tickets.length; i++){
-            if(start.equals(tickets[i][0]) && !visited[i]){
+        // 모든 티켓을 사용했을 경우
+        if (depth == tickets.length){
+            answer = route.split(" ");
+            found = true;
+            return;
+        }
+        
+        for (int i = 0; i < tickets.length; i++){
+            if (start.equals(tickets[i][0]) && !visited[i]){
                 visited[i] = true;
-                dfs(tickets[i][1], route+" "+tickets[i][1], tickets, cnt+1);
+                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, depth + 1);
                 visited[i] = false;
             }
         }
     }
-    
 }
