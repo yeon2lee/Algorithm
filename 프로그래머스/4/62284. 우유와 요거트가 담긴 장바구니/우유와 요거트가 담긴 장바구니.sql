b26f9f2f3b70f94c41b9f12833cf95cb53ee509d
Nayeon Lee <77628363+yeon2lee@users.noreply.github.com>
2025-09-27 20:10:02 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,12 @@
+-- 우유와 요거트를 동시에 구입한 장바구니의 아이디를 조회
+
+SELECT CART_ID
+FROM CART_PRODUCTS
+WHERE NAME = 'Milk'
+    
+INTERSECT 
+    
+SELECT CART_ID
+FROM CART_PRODUCTS
+WHERE NAME = 'Yogurt'
+ORDER BY CART_ID
